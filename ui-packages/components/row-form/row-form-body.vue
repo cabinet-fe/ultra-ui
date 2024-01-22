12ac1943a943c79cd69bbc7261a9430ab58fac52
赵金鹏 <z15044356261@163.com>
2024-01-22 18:28:58 +0800

    chore: ts类型

---
+++ ui-packages/components/row-form/row-form-body.vue
@@ -0,0 +1,24 @@
+<template>
+  <tbody :class="store.cls.e('tbody')">
+    <tr
+      :class="store.cls.e('tr')"
+      v-for="(dataItem, dataIndex) in store.data"
+      :key="dataIndex"
+    >
+      <!-- 内容 -->
+      <td
+        v-for="(columnsItem, columnsIndex) in store.columns"
+        :key="columnsIndex"
+      >
+        <slot ref="slotsRef" :name="columnsItem.key" :row="dataItem" />
+      </td>
+    </tr>
+  </tbody>
+</template>
+<script lang="ts" setup>
+import { inject } from 'vue'
+import { RowFormStoreType } from './row-form.type'
+
+let store = inject(RowFormStoreType)!
+</script>
+<style></style>
