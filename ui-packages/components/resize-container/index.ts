cd5e09c5720691bded6c489d3b2af5d3f836dd3e
whj <emailwen@qq.com>
2024-02-21 17:22:14 +0800

    feat: 新增可调尺寸容器组件

---
+++ ui-packages/components/resize-container/index.ts
@@ -0,0 +1,7 @@
+export { default as UResizeContainer } from './resize-container.vue'
+
+export type {
+  ResizeContainerProps,
+  ResizeContainerEmits,
+  ResizeContainerExposed
+} from '@ui/types/components/resize-container'
---
+++ ui-packages/components/resize-container/resize-container.vue
@@ -0,0 +1,16 @@
+<template>
+  <div :class="cls.b"></div>
+</template>
+
+<script lang="ts" setup>
+import type { ResizeContainerProps } from '@ui/types/components/resize-container'
+import { bem } from '@ui/utils'
+
+defineOptions({
+  name: 'ResizeContainer'
+})
+
+defineProps<ResizeContainerProps>()
+
+const cls = bem('resize-container')
+</script>
