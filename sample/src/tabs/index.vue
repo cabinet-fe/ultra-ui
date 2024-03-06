d2aa1fae927ec0934d484679529dbd5849fae5a2
snowerskk <914637371@qq.com>
2024-03-06 16:50:56 +0800

    chore: 其他修改

--- sample/src/tabs/index.vue
+++ sample/src/tabs/index.vue
@@ -1,30 +1,50 @@
 <template>
-  <ul class="list">
-    <li class="item" v-for="(position, index) in positions">
+  <div class="wrapper">
+    <div class="config">
+      <div class="title">config</div>
+      <ul>
+        <li v-for="item in configList">
+          <u-checkbox v-model="config[item.key]">{{ item.label }}：{{ item.key }}</u-checkbox>
+        </li>
+        <li>
+          <div>方位：position</div>
+          <u-radio-group
+            radioType="btn"
+            :data="[
+              { label: '上', value: 'top' },
+              { label: '下', value: 'bottom' },
+              { label: '左', value: 'left' },
+              { label: '右', value: 'right' }
+            ]"
+            v-model="config.position"
+          />
+        </li>
+      </ul>
+    </div>
+    <div class="display">
+      <div class="title">display</div>
       <u-tabs
         :items="items"
-        v-model="active[index]"
-        :position="position"
-        :closable="true"
+        v-model="active"
+        :position="config.position"
+        :closable="config.closable"
         @click="handleClick"
         @delete="handleDelete"
         @update:items="handleUpdate"
-        :sortable="true"
+        :sortable="config.sortable"
       >
         <template v-for="item in items" #[item]>{{ item }}</template>
       </u-tabs>
-    </li>
-  </ul>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 let items = ref(['TabOne', 'TabTwo', 'TabThree', 'TabFour'])
 
-const active = ref<string[]>(['TabOne', 'TabOne', 'TabOne', 'TabOne'])
-
-const positions: any[] = ['top', 'bottom', 'left', 'right']
+const active = ref<string>('TabOne')
 
 const handleClick = (item, index) => {
   console.log(item, index)
@@ -37,18 +57,35 @@ const handleDelete = (item, index) => {
 const handleUpdate = (item) => {
   console.log(item)
 }
+
+const configList = [
+  { label: '可关闭', key: 'closable' },
+  { label: '排序', key: 'sortable' }
+]
+const config = reactive({
+  closable: true,
+  sortable: false,
+  position: 'top' as any
+})
 </script>
 
 <style lang="scss" scoped>
-.list {
+.wrapper {
+  height: 100%;
   display: flex;
   justify-content: space-around;
-  flex-wrap: wrap;
-  .item {
-    width: 500px;
-    height: 200px;
-    border: 1px solid #eee;
-    margin: 20px;
+  .config {
+    flex: 1;
+    border-right: 1px solid #eee;
+  }
+  .display {
+    flex: 2;
+  }
+  .title {
+    font-size: 20px;
+    font-weight: 700;
+    text-align: center;
+    line-height: 50px;
   }
 }
 </style>
