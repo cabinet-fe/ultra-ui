4deac5da88f05d98bda3b46029d4e4b3fd687f9f
whj <emailwen@qq.com>
2024-03-15 15:37:27 +0800

    feat: 完成dialog组件

--- ui-packages/compositions/use-form-component/index.ts
+++ ui-packages/compositions/use-form-component/index.ts
@@ -9,21 +9,19 @@ const FormComponentDIKey: InjectionKey<DIContext> = Symbol('FormComponentDIKey')
 
 /**
  * 表单组件本身的组合式方法
- * @param isForm 是否为form组件
  * @returns
  */
-export function useFormComponent(isForm: true, props: FormProps): void
+export function useFormComponent(props: FormProps): void
 /**
  * 表单内的组件的组合式方法
- * @param isForm 是否为form组件
  * @returns 提供一个form的上下文
  */
-export function useFormComponent(isForm: false): {
-    /** 是否在表单中 */
-   inForm: boolean
- } & (DIContext | undefined)
-export function useFormComponent(isForm: boolean, props?: any) {
-  if (isForm) {
+export function useFormComponent(): {
+  /** 是否在表单中 */
+  inForm: boolean
+} & Partial<DIContext>
+export function useFormComponent(props?: any): any {
+  if (props) {
     return provide(FormComponentDIKey, {
       formProps: props
     })
