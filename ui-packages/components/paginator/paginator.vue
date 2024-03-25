55787d18ada56cbd6e1d09e7ae3b698f1042a52a
snowerskk <914637371@qq.com>
2024-03-25 13:39:29 +0800

    feat: 新增分页器组件

---
+++ ui-packages/components/paginator/paginator.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="cls.b">paginator</div>
+</template>
+
+<script lang="ts" setup>
+import type { PaginatorProps } from '@ui/types/components/paginator'
+import { bem } from '@ui/utils'
+import { useFormFallbackProps, useFormComponent } from '@ui/compositions'
+
+defineOptions({
+  name: 'Paginator'
+})
+
+const props = withDefaults(defineProps<PaginatorProps>(), {
+
+})
+
+const { formProps } = useFormComponent()
+
+const {  } = useFormFallbackProps([
+  formProps ?? {},
+  props
+])
+
+
+const cls = bem('paginator')
+</script>
