57c2e74ee43e1bbfcf8fae9c5c4781f64e66d07f
snowerskk <914637371@qq.com>
2024-03-26 15:40:06 +0800

    feat: 水平步骤条

--- sample/src/steps/index.vue
+++ sample/src/steps/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <u-steps></u-steps>
+  <u-steps v-model:active="active" :items="items"></u-steps>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const active = ref<string>('2')
+
+let items = ref([
+  { label: '开始', key: '1' },
+  { label: '过程x', key: '2' },
+  { label: '过程y', key: '3' },
+  { label: '结束', key: '4' }
+])
+</script>
--- ui-packages/types/components/steps.d.ts
+++ ui-packages/types/components/steps.d.ts
@@ -1,13 +1,22 @@
 import type { DeconstructValue } from '../helper'
 
+interface Item {
+  label: string
+  key: string
+}
+
 /** 步骤组件组件属性 */
 export interface StepsProps {
   modelValue?: string
+  active: string
+  items: Item[]
+  mode: 'horizontal' | 'vertical'
 }
 
 /** 步骤组件组件定义的事件 */
 export interface StepsEmits {
   (e: 'update:modelValue', value: string): void
+  (e: 'update:active', value: string): void
 }
 
 /** 步骤组件组件暴露的属性和方法(组件内部使用) */
