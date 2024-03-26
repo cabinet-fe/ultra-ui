55b94b824fa411a72de82f7abbbf901ca2f9a063
snowerskk <914637371@qq.com>
2024-03-26 13:46:20 +0800

    feat: 新增steps组件

---
+++ sample/src/steps/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <u-steps></u-steps>
+</template>
+
+<script setup lang="ts"></script>
---
+++ ui-packages/components/steps/steps.vue
@@ -0,0 +1,16 @@
+<template>
+  <div :class="cls.b">steps</div>
+</template>
+
+<script lang="ts" setup>
+import type { StepsProps } from '@ui/types/components/steps'
+import { bem } from '@ui/utils'
+
+defineOptions({
+  name: 'Steps'
+})
+
+defineProps<StepsProps>()
+
+const cls = bem('steps')
+</script>
