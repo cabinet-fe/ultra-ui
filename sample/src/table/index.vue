ff931a0b5456982ed50686fdefc667c25ad0886b
whj <emailwen@qq.com>
2024-03-26 17:11:44 +0800

    chore: 更新cat-kit依赖

--- sample/src/table/index.vue
+++ sample/src/table/index.vue
@@ -7,13 +7,18 @@
 </template>
 
 <script lang="ts" setup>
+import { defineTableColumns } from 'ultra-ui'
 import { shallowRef } from 'vue'
 
-const columns = [
+const columns = defineTableColumns([
   { name: '姓名', key: 'name' },
   { name: '年龄', key: 'age' },
-  { name: '地址', key: 'address' }
-]
+  { name: '地址', key: 'address', children: [
+    { name: '省', key: 'province' },
+    { name: '市', key: 'city' },
+    { name: '区', key: 'area' },
+  ] }
+])
 
 const data = shallowRef(Array.from({ length: 20 }).map((_, index) => {
   return ({
