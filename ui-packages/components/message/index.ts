56ee9087634c084b7b684e6eb609fd0e73e7cf64
snowerskk <914637371@qq.com>
2024-04-02 15:15:27 +0800

    feat: message基础功能

--- ui-packages/components/message/index.ts
+++ ui-packages/components/message/index.ts
@@ -1,7 +1,26 @@
 export { default as UMessage } from './message.vue'
+import UMessage from './message.vue'
+import { createVNode, render, type VNode, watch } from 'vue'
 
 export type {
   MessageProps,
   MessageEmits,
   MessageExposed
 } from '@ui/types/components/message'
+
+type MessageItem = {
+  vm: VNode
+}
+
+const messageQueue: MessageItem[] = []
+
+let offset = -32
+
+export const Message = (options={}) => {
+  const container = document.createElement('div')
+  offset += 52
+  const vm = createVNode(UMessage, { ...options, style: { top: `${offset}px` }})
+  render(vm, container)
+  messageQueue.push({ vm })
+  document.body.appendChild(container.firstElementChild!)
+}
