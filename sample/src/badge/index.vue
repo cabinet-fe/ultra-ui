04ae39b2c9b43a0dda770b6438aeaa485aced16f
付东飞 <fdf9758@163.com>
2024-04-02 17:09:00 +0800

    feat: badge组件

--- sample/src/badge/index.vue
+++ sample/src/badge/index.vue
@@ -1,11 +1,34 @@
 <template>
   <div>
-    <u-badge>
-      <u-button :value="12">通知</u-button>
-    </u-badge>
+    <div class="badge-demo">
+      <u-badge :value="5">
+        <u-button>more</u-button>
+      </u-badge>
+      <u-badge :value="10" type="primary">
+        <u-button>primary</u-button>
+      </u-badge>
+      <u-badge :value="15" type="warning">
+        <u-button>warning</u-button>
+      </u-badge>
+      <u-badge :value="120" color="green">
+        <u-button>color</u-button>
+      </u-badge>
+      <u-badge dot>
+        未读消息
+      </u-badge>
+      <u-badge :value="120" hidden>
+        <u-button>hidden</u-button>
+      </u-badge>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.badge-demo {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
