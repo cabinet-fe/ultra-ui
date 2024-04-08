bd0fd7f407666515caa574a9692f36a339bfd2c4
赵金鹏 <z15044356261@163.com>
2024-04-08 11:27:02 +0800

    feat: 新增row-form删除和插入

--- ui-packages/components/row-form/row-form.vue
+++ ui-packages/components/row-form/row-form.vue
@@ -7,7 +7,7 @@
     >
       <div
         v-if="useSlots()['column:' + item.key]"
-        @click="e => handleClick(e, row.index)"
+        @click="(e: Event) => handleClick(e, row.index)"
       >
         <node-render
           :content="
@@ -19,9 +19,18 @@
       <!-- 操作栏 -->
       <template v-else-if="item.key === 'operation'">
         <button-common-props tag="span">
-          <u-button :class="cls.m('interval')" :icon="Delete" type="primary" />
+          <u-button
+            :class="cls.m('interval')"
+            :icon="Delete"
+            type="primary"
+            @click="handleDelRows(row.index)"
+          />
 
-          <u-button :icon="DocumentAdd" type="primary" />
+          <u-button
+            :icon="DocumentAdd"
+            type="primary"
+            @click="handleInsetToRows(row.index)"
+          />
         </button-common-props>
       </template>
 
@@ -43,6 +52,7 @@ import { Delete, DocumentAdd } from 'icon-ultra'
 import { UButton } from '../button'
 import { bem } from '@ui/utils'
 import { useComponentProps } from '@ui/compositions'
+import { useOperation } from './use-operation'
 
 /** 接收的参数 */
 const props = defineProps<RowFormProps<T>>()
@@ -97,6 +107,8 @@ watch(rows, val => {
   data.value = rows.value
 })
 
+const { insetTo, delRow } = useOperation()
+
 let obj = {
   clickIndex: ref(0)
 }
@@ -110,9 +122,9 @@ const handleClick = (e: Event, index: number) => {
 
 /** 失去焦点 */
 const handleBlurEvent = (e: Event) => {
+  /** 最后一条失去焦点就新增一条 */
   if (rows.value.length - 1 === obj.clickIndex.value) {
-    let row = {}
-    rows.value = wrapDataRows([...rows.value, row]) as T[]
+    rows.value = insetTo(rows.value, obj.clickIndex.value) as T[]
   }
 
   /** 结束时候清除事件 */
@@ -125,8 +137,18 @@ const handleInputEvent = (e: Event) => {
   data.value = rows.value
 }
 
+/** 插入rows */
+const handleInsetToRows = (index: number) => {
+  rows.value = insetTo(rows.value, index) as T[]
+}
+
+/** 删除 */
+const handleDelRows = (index: number) => {
+  rows.value = delRow(rows.value, [index]) as T[]
+
+}
+
 const getRowFormSlotsNodes = (key: string, options: Option) => {
-  console.log(key, 'keykey')
   return useSlots()!['column:' + key]?.({ ...options })
 }
 </script>
