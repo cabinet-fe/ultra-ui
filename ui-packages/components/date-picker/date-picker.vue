5d81b4cffceb0f4d5d1512bbdd3a1b2597969654
whj <emailwen@qq.com>
2024-04-09 16:40:33 +0800

    feat: 新增日期组件

---
+++ ui-packages/components/date-picker/date-picker.vue
@@ -0,0 +1,69 @@
+<template>
+  <u-dropdown
+    :class="cls.b"
+    trigger="click"
+    min-width="300px"
+    :content-class="cls.e('panel')"
+  >
+    <template #trigger>
+      <u-input :size="size" />
+    </template>
+
+    <template #content>
+      <div >
+        <span></span>
+        <span></span>
+
+        <span>
+          2024
+        </span>
+        <span>
+          4月
+        </span>
+
+
+        <span></span>
+        <span></span>
+      </div>
+      <ul :class="cls.e('days')">
+        <li :class="cls.e('day')" v-for="day of days">
+          {{ day }}
+        </li>
+      </ul>
+    </template>
+  </u-dropdown>
+</template>
+
+<script lang="ts" setup>
+import type { DatePickerProps } from '@ui/types/components/date-picker'
+import { bem } from '@ui/utils'
+import { UDropdown } from '../dropdown'
+import { UInput } from '../input'
+import { useFormComponent, useFormFallbackProps } from '@ui/compositions'
+import { shallowRef } from 'vue'
+import { date } from 'cat-kit/fe'
+
+defineOptions({
+  name: 'DatePicker'
+})
+
+const props = withDefaults(defineProps<DatePickerProps>(), {})
+
+const cls = bem('date-picker')
+
+const { formProps } = useFormComponent()
+
+const { size } = useFormFallbackProps([formProps ?? {}, props], {
+  size: 'small'
+})
+
+const days = shallowRef(Array.from({ length: 42 }).fill(0))
+
+function getMonthDays(d: Date) {
+  // 跳转到当月的第一天
+  d.setDate(1)
+  return 7 - d.getDay()
+}
+
+console.log(getMonthDays(new Date()))
+</script>
---
+++ ui-packages/types/components/date-picker.d.ts
@@ -0,0 +1,21 @@
+import type { DeconstructValue } from '../helper'
+
+/** date-picker组件属性 */
+export interface DatePickerProps {
+  modelValue?: string
+}
+
+/** date-picker组件定义的事件 */
+export interface DatePickerEmits {
+  (e: 'update:modelValue', value: string): void
+}
+
+export interface Day {
+  num: number
+}
+
+/** date-picker组件暴露的属性和方法(组件内部使用) */
+export interface _DatePickerExposed {}
+
+/** date-picker组件暴露的属性和方法(组件外部使用, 引用的值会被自动解构) */
+export type DatePickerExposed = DeconstructValue<_DatePickerExposed>
