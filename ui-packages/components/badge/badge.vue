3f84ce7d7114c7f54160e0f215aca021917afd44
whj <emailwen@qq.com>
2024-04-10 15:49:56 +0800

    refactor: 一些样式更新

--- ui-packages/components/badge/badge.vue
+++ ui-packages/components/badge/badge.vue
@@ -6,43 +6,48 @@
 </template>
 
 <script lang="ts" setup>
-import {computed, nextTick, onMounted, shallowRef, watch} from "vue"
-import type {BadgeProps} from "@ui/types/components/badge"
-import {bem, setStyles} from "@ui/utils"
-import {useFallbackProps} from "@ui/compositions"
-import type {ComponentSize} from "@ui/types/component-common"
+import { computed, nextTick, onMounted, shallowRef, watch } from 'vue'
+import type { BadgeProps } from '@ui/types/components/badge'
+import { bem, setStyles } from '@ui/utils'
+import { useFallbackProps } from '@ui/compositions'
+import type { ComponentSize } from '@ui/types/component-common'
 
 defineOptions({
-  name: "Badge",
+  name: 'Badge'
 })
 
-const cls = bem("badge")
+const cls = bem('badge')
 
 const props = withDefaults(defineProps<BadgeProps>(), {
   max: 99
 })
 
-const {size} = useFallbackProps([props], {
-  size: "default" as ComponentSize,
+const { size } = useFallbackProps([props], {
+  size: 'default' as ComponentSize
 })
 
 const badgeValue = computed(() => {
-  if(props.dot) return ''
+  if (props.dot) return ''
   return props.value! > props.max ? `${props.max}+` : props.value
 })
 
 const classList = computed(() => {
-  return [cls.e("sup"), props.type && cls.m(props.type), cls.m(size.value),bem.is('dot',props.dot)]
+  return [
+    cls.e('sup'),
+    props.type && cls.m(props.type),
+    cls.m(size.value),
+    bem.is('dot', props.dot)
+  ]
 })
 
 const supRef = shallowRef<HTMLElement>()
 
 const setPosition = () => {
   if (supRef.value) {
-    const {width, height} = supRef.value.getBoundingClientRect()
+    const { width, height } = supRef.value.getBoundingClientRect()
     setStyles(supRef.value, {
       transform: `translate(-${width / 2}px, -${height / 2}px)`,
-      backgroundColor: props.color && props.color,
+      backgroundColor: props.color && props.color
     })
   }
 }
