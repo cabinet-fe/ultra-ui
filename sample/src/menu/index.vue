518cbee5aee8b0fa109bde2f9d80cbc47efada84
snowerskk <914637371@qq.com>
2024-04-25 17:43:20 +0800

    feat: 新增menu组件

---
+++ sample/src/menu/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <u-menu>
+    <u-menu-sub>
+      <template #title>
+        <u-icon><Location /></u-icon>
+        <span>Navigator One</span>
+      </template>
+      <u-menu-item>item one</u-menu-item>
+    </u-menu-sub>
+    <u-menu-item />
+  </u-menu>
+</template>
+
+<script setup lang="ts">
+import { Location } from 'icon-ultra'
+</script>
