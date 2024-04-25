b120d6154f329dfcf3abaf157690d54a4e43ad1a
whj <emailwen@qq.com>
2024-04-25 13:59:01 +0800

    dx: 增加提交校验

---
+++ cli/verify-commit/index.ts
@@ -0,0 +1,19 @@
+import pico from 'picocolors'
+import { readFileSync } from 'node:fs'
+import path from 'node:path'
+
+const msgPath = path.resolve('.git/COMMIT_EDITMSG')
+const msg = readFileSync(msgPath, 'utf-8').trim()
+
+const commitRE =
+  /^(feat|fix|docs|dx|style|refactor|perf|build|ci|chore|types|release)(\(.+\))?: .{1,50}/
+
+if (!commitRE.test(msg)) {
+  console.error(
+    `  ${pico.white(pico.bgRed(' ERROR '))} ${pico.red(
+      `提交格式不正确。请查看README.md中的提交规范获取更多信息。`
+    )}
+    `
+  )
+  process.exit(1)
+}
