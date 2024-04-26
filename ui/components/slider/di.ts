caaf052e34e4e10403921f19fb5ebccca368ffe0
赵金鹏 <z15044356261@163.com>
2024-04-26 17:37:25 +0800

    fix: slider inject cls类型

--- ui/components/slider/di.ts
+++ ui/components/slider/di.ts
@@ -1,7 +1,4 @@
-import type {
-  SliderEmits,
-  SliderProps
-} from '@ui/types/components/slider'
+import type { SliderEmits, SliderProps } from '@ui/types/components/slider'
 import type { BEM } from '@ui/utils'
 import type { InjectionKey, ModelRef, ShallowRef } from 'vue'
 
@@ -14,13 +11,13 @@ import type { InjectionKey, ModelRef, ShallowRef } from 'vue'
 // }
 
 export interface SliderContext {
-  cls: BEM<'slider', 'u-slider'>
+  cls: BEM<'slider', 'u-', 'u-slider'>
   runwayRef: ShallowRef<HTMLElement | undefined>
   sliderSize: ShallowRef<number>
   emit: SliderEmits
   sliderProps: SliderProps
-  model: ModelRef<number | number[] | undefined>,
-  setSliderBarSize: ({ x, y }: { x: number; y: number; }) => void
+  model: ModelRef<number | number[] | undefined>
+  setSliderBarSize: ({ x, y }: { x: number; y: number }) => void
 }
 
 export const sliderContextKey: InjectionKey<SliderContext> =
