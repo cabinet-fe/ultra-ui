8a3470bfe9199ecc4faf7de30c14084115e2b0aa
snowerskk <914637371@qq.com>
2024-04-26 16:06:43 +0800

    feat: menu基础功能

--- sample/src/menu/index.vue
+++ sample/src/menu/index.vue
@@ -1,16 +1,61 @@
 <template>
-  <u-menu>
-    <u-menu-sub>
+  <div>
+    <div>
+      <u-button @click="handleOpen" type="primary">open</u-button>
+      <u-button @click="handleClose" type="primary">close</u-button>
+    </div>
+    <u-menu @open="open" @close="close" ref="menuRef">
+    <u-menu-sub index="111">
       <template #title>
         <u-icon><Location /></u-icon>
         <span>Navigator One</span>
       </template>
       <u-menu-item>item one</u-menu-item>
+      <u-menu-item>item one</u-menu-item>
+      <u-menu-item>item one</u-menu-item>
+      <u-menu-item>item one</u-menu-item>
+      <u-menu-item>item one</u-menu-item>
+      <u-menu-item>item one</u-menu-item>
+      <u-menu-item>item one</u-menu-item>
+      <u-menu-sub index="222">
+        <template #title>
+          <u-icon><Download /></u-icon>
+          <span>次级标题</span>
+        </template>
+        <u-menu-item>1-1</u-menu-item>
+        <u-menu-item>1-2</u-menu-item>
+      </u-menu-sub>
     </u-menu-sub>
-    <u-menu-item />
+    <u-menu-item>1</u-menu-item>
+    <u-menu-item>1</u-menu-item>
+    <u-menu-item>1</u-menu-item>
+    <u-menu-item>1</u-menu-item>
+    <u-menu-item>1</u-menu-item>
+    <u-menu-item>1</u-menu-item>
   </u-menu>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { Location } from 'icon-ultra'
+import { Location, Download } from 'icon-ultra'
+import type { UMenu } from 'ultra-ui/components'
+import { shallowRef } from 'vue'
+
+const open = (index: string) => {
+  console.log('open', index)
+}
+
+const close = (index: string) => {
+  console.log('close', index)
+}
+
+const menuRef = shallowRef<InstanceType<typeof UMenu>>()
+
+const handleOpen = () => {
+  menuRef.value?.open('111')
+}
+
+const handleClose = () => {
+  menuRef.value?.close('111')
+}
 </script>
--- ui/components/menu/menu.vue
+++ ui/components/menu/menu.vue
@@ -1,12 +1,13 @@
 <template>
-  <div :class="cls.b"><slot /></div>
+  <div :class="[cls.b, cls.m(size)]"><slot /></div>
 </template>
 
 <script lang="ts" setup>
-import type { MenuProps } from '@ui/types/components/menu'
+import type { MenuEmits, MenuExposed, MenuProps } from '@ui/types/components/menu'
 import { bem } from '@ui/utils'
-import { useSlots, provide } from 'vue'
-import { MenuDIKey } from './di'
+import { provide, inject, ref, shallowRef } from 'vue'
+import { MenuDIKey, type MenuContext } from './di'
+import { useFallbackProps } from '@ui/compositions'
 
 defineOptions({
   name: 'Menu'
@@ -14,9 +15,34 @@ defineOptions({
 
 const props = defineProps<MenuProps>()
 
+const emit = defineEmits<MenuEmits>()
+
 const cls = bem('menu')
 
-const slots = useSlots()
+const { size } = useFallbackProps([props], {
+  size: 'default'
+})
 
-provide(MenuDIKey, { cls, menuProps: props })
+const store = shallowRef<MenuContext>({
+  cls,
+  menuProps: props,
+  menuEmit: emit,
+  menuSubs: {}
+})
+
+provide(MenuDIKey, store.value )
+
+const open = (index: string) => {
+  if (store.value.menuSubs[index]) {
+    (store.value.menuSubs[index]!.exposed as any).open()
+  }
+}
+
+const close = (index: string) => {
+  if (store.value.menuSubs[index]) {
+    (store.value.menuSubs[index]!.exposed as any).close()
+  }
+}
+
+defineExpose({ open, close })
 </script>
