e18f493ae7a4b5fa78b3125af4ef0b17e7795744
空白03 <1664930915@qq.com>
2024-04-28 18:21:16 +0800

    feat: 水印组件

--- sample/src/watermark/index.vue
+++ sample/src/watermark/index.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
     <div>水印</div>
-    <!-- <u-watermark> 111111 </u-watermark> -->
-    <u-watermark
+    <u-watermark text="watermark" font-size="60" font-color="#fff">
+      <p v-for="item in list">
+        {{ item }}
+      </p>
+    </u-watermark>
+
+    <!-- <u-watermark
       image="https://q9.itc.cn/q_70/images01/20240325/c8916e957ea944a38df28e911e117a5f.png"
     >
       111111
-    </u-watermark>
+      <p v-for="item in list">
+        {{ item }}
+      </p>
+    </u-watermark> -->
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import type Watermark from 'ultra-ui/components/watermark/watermark.vue'
+
+const list = []
+for (var i = 0; i <= 100; i++) {
+  list.push(
+    '水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印水印'
+  )
+}
+</script>
 
 <style lang="scss" scoped></style>
