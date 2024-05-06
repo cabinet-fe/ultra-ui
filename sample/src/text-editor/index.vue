88bda163a68fb64b2b2c18a555a9ac77fe4b261a
赵金鹏 <z15044356261@163.com>
2024-05-06 17:35:29 +0800

    feat: editor.js下载list,checked插件

--- sample/src/text-editor/index.vue
+++ sample/src/text-editor/index.vue
@@ -1,7 +1,53 @@
 <template>
-    <UTextEditor></UTextEditor>
+  <div>
+    <div>
+      {{ modelValue }}
+      <UTextEditor
+        v-model="modelValue"
+        height="500px"
+        @change="handleChangeEditor"
+      ></UTextEditor>
+    </div>
+
+    export interface TextEditorProps {
+      modelValue?: TextEditorOutputData
+      /** 高度 */
+      height?: string
+      width?: string
+      disabled?: boolean
+    }
+  </div>
 </template>
 <script lang="ts" setup>
+import { ref } from 'vue'
 
+let modelValue = ref({
+  blocks: [
+    {
+      id: '0QHrITflqv',
+      type: 'paragraph',
+      data: {
+        text: '123123'
+      }
+    },
+    {
+      id: 'BtEbs-4fqc',
+      type: 'paragraph',
+      data: {
+        text: '123321'
+      }
+    },
+    {
+      type: 'paragraph',
+      data: {
+        text: '<b>123123123123</b>'
+      }
+    }
+  ]
+})
+
+const handleChangeEditor = (value, api, event) => {
+  console.log(value, api, event)
+}
 </script>
 <style></style>
