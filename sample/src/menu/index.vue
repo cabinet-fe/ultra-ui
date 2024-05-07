06acccc83578cf558f620d5c1a66d35f9d635a5f
snowerskk <914637371@qq.com>
2024-05-07 20:14:47 +0800

    style: 优化disabled状态样式

--- sample/src/menu/index.vue
+++ sample/src/menu/index.vue
@@ -35,7 +35,7 @@
           </u-menu-sub>
         </u-menu-sub>
         <u-menu-item index="a1" :icon="Setting">一级菜单2</u-menu-item>
-        <u-menu-sub index="a2" :icon="Setting">
+        <u-menu-sub index="a2" :icon="Setting" disabled>
           <template #title>一级菜单3</template>
           <u-menu-item index="a21">二级菜单1</u-menu-item>
           <u-menu-item index="a22">二级菜单1</u-menu-item>
@@ -43,7 +43,7 @@
         </u-menu-sub>
         <u-menu-item index="a3" :icon="Setting">一级菜单4</u-menu-item>
         <u-menu-item index="a4" :icon="Setting">一级菜单5</u-menu-item>
-        <u-menu-item index="a5" :icon="Setting">一级菜单6</u-menu-item>
+        <u-menu-item index="a5" :icon="Setting" disabled>一级菜单6</u-menu-item>
         <u-menu-item index="a6" :icon="Setting">一级菜单7</u-menu-item>
       </u-menu>
     </u-scroll>
