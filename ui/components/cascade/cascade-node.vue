e6588f888a08c2d7be6527cc84d23607606158a0
付东飞 <fdf9758@163.com>
2024-07-10 15:11:27 +0800

    feat: cascade  优化

--- ui/components/cascade/cascade-node.vue
+++ ui/components/cascade/cascade-node.vue
@@ -30,7 +30,6 @@
                 @update:model-value="handleCheck(option, $event)"
               ></u-checkbox>
               {{ option.data[labelKey!] }}
-              {{ option.data[valueKey!] }}
             </div>
             <u-icon v-if="option[childrenKey!] && option[childrenKey!].length"
               ><ArrowRight
