464a9f8b99bc3cef2a88ac2ceab2c8d89c602381
付东飞 <fdf9758@163.com>
2024-08-08 14:10:06 +0800

    feat: 级联筛选选中效果优化

--- sample/components.d.ts
+++ sample/components.d.ts
@@ -9,21 +9,11 @@ declare module 'vue' {
   export interface GlobalComponents {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    UBadge: typeof import('ultra-ui')['UBadge']
-    UBatchEdit: typeof import('ultra-ui')['UBatchEdit']
-    UButton: typeof import('ultra-ui')['UButton']
     UCard: typeof import('ultra-ui')['UCard']
     UCardContent: typeof import('ultra-ui')['UCardContent']
     UCardHeader: typeof import('ultra-ui')['UCardHeader']
     UCascade: typeof import('ultra-ui')['UCascade']
-    UCheckbox: typeof import('ultra-ui')['UCheckbox']
-    UDialog: typeof import('ultra-ui')['UDialog']
-    UInput: typeof import('ultra-ui')['UInput']
-    UMultiTreeSelect: typeof import('ultra-ui')['UMultiTreeSelect']
-    UNumberInput: typeof import('ultra-ui')['UNumberInput']
     URadio: typeof import('ultra-ui')['URadio']
     UScroll: typeof import('ultra-ui')['UScroll']
-    USelect: typeof import('ultra-ui')['USelect']
-    UTreeSelect: typeof import('ultra-ui')['UTreeSelect']
   }
 }
