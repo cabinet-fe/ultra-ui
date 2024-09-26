70880ac3a22e5fa44f072b55ee89c43bb9667920
whj <emailwen@qq.com>
2024-09-26 18:08:25 +0800

    chore: 杂项

--- cli/verify-commit/index.ts
+++ cli/verify-commit/index.ts
@@ -1,4 +1,3 @@
-import pico from 'picocolors'
 import { readFileSync } from 'node:fs'
 import path from 'node:path'
 
@@ -9,9 +8,6 @@ const commitRE =
   /^(feat|fix|docs|dx|style|refactor|perf|build|ci|chore|types|release)(\(.+\))?: .{1,50}/
 
 if (!commitRE.test(msg)) {
-  console.error(
-    `  ${pico.white(pico.bgRed(' ERROR '))} ${pico.red(`提交格式不正确。`)}
-    `
-  )
+  console.error(`[错误]: 提交格式不正确。`)
   process.exit(1)
 }
