7ceb52150ba07d568e99c4119b9dd0a42f5a7a94
whj <emailwen@qq.com>
2024-10-28 16:37:16 +0800

    feat: 现在组件提供一个全局导出

---
+++ build/gen-install.ts
@@ -0,0 +1,43 @@
+import fg from 'fast-glob'
+import { UI_ROOT } from './helper'
+import { resolve } from 'node:path'
+
+export async function genInstall() {
+  const entries = await fg.glob(
+    ['components/**/*.scss', 'directives/**/*.scss'],
+    {
+      ignore: ['**/node_modules', '**/_*.scss'],
+      cwd: UI_ROOT
+    }
+  )
+
+  const stylesImports = entries.map(entry => `import './${entry}'`).join('\n')
+
+  const installScripts = `
+import type { App } from 'vue'
+import * as components from './components'
+import * as directives from './directives'
+
+// 引入样式
+import './styles'
+${stylesImports}
+
+export function UltraUI(app: App) {
+  Object.keys(components).forEach(key => {
+    if (key.startsWith('U')) {
+      const component = components[key]
+      component && app.component(key, component)
+    }
+  })
+
+  Object.keys(directives).forEach(key => {
+    if (key.startsWith('v')) {
+      const directive = directives[key]
+      directive && app.directive(key.slice(1), directive)
+    }
+  })
+}
+`
+
+  await Bun.write(resolve(UI_ROOT, 'install.ts'), installScripts)
+}
