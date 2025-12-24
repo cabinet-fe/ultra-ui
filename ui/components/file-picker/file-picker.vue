8e58c2380e349b242916ba0e8f9e56552f8890f3
whj <emailwen@qq.com>
2025-12-24 15:10:11 +0800

    release: 🎉 ultra发布 0.4.23 版本

--- ui/components/file-picker/file-picker.vue
+++ ui/components/file-picker/file-picker.vue
@@ -1,5 +1,12 @@
 <template>
-  <component :is="tag" :class="cls.b" @click.stop="handleClickPicker">
+  <component
+    :is="tag"
+    :class="cls.b"
+    @click.stop="handleClickPicker"
+    @dragover.prevent="handleDragover"
+    @dragleave="handleDragLeave"
+    @drop.prevent="handleDrop"
+  >
     <input
       :multiple="multiple"
       :class="cls.e('native')"
@@ -11,14 +18,14 @@
       @change="handleChange"
       capture="environment"
     />
-    <slot />
+    <slot :is-dragover="isDragover" />
   </component>
 </template>
 
 <script lang="ts" setup>
 import type { UploaderProps, UploaderEmits } from '@ui/types'
 import { bem } from '@ui/utils'
-import { shallowRef } from 'vue'
+import { shallowRef, ref } from 'vue'
 import { matchAccept } from './helper'
 
 defineOptions({
@@ -35,14 +42,39 @@ const emit = defineEmits<UploaderEmits>()
 const cls = bem('file-picker')
 
 const fileRef = shallowRef<HTMLInputElement>()
+const isDragover = ref(false)
+
+const processFiles = (files: File[]) => {
+  const filteredFiles = files.filter(file => matchAccept(file, props.accept))
+  emit('pick', filteredFiles)
+}
 
 const handleChange = (e: Event) => {
   const target = e.target as HTMLInputElement
-  const files = Array.prototype.slice.call(target.files) as File[]
-  const filteredFiles = files.filter(file => matchAccept(file, props.accept))
-
-  emit('pick', filteredFiles)
+  const files = Array.from(target.files || [])
+  processFiles(files)
   target.value = ''
+}
+
+const handleDragover = (e: DragEvent) => {
+  if (props.disabled) return
+  isDragover.value = true
+}
+
+const handleDragLeave = (e: DragEvent) => {
+  if (props.disabled) return
+  const currentTarget = e.currentTarget as HTMLElement
+  const relatedTarget = e.relatedTarget as HTMLElement | null
+
+  if (currentTarget.contains(relatedTarget)) return
+  isDragover.value = false
+}
+
+const handleDrop = (e: DragEvent) => {
+  if (props.disabled) return
+  isDragover.value = false
+  const files = Array.from(e.dataTransfer?.files || [])
+  processFiles(files)
 }
 
 function handleClickPicker() {
