382b7ebfb372bcaa3022d422cad556a2ac53ea04
whj <emailwen@qq.com>
2026-04-10 17:34:08 +0800

    refactor: 大重构

--- playgrounds/icons/vite.config.ts
+++ playgrounds/icons/vite.config.ts
@@ -1,22 +1,4 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const iconsRoot = resolve(__dirname, '../../packages/icons/src')
-const iconsVueRoot = resolve(iconsRoot, 'vue')
-
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: [
-      { find: /^@ultra-ui\/icons\/vue\/(.+)$/, replacement: `${iconsVueRoot}/$1.vue` },
-      { find: /^@ultra-ui\/icons\/normal$/, replacement: resolve(iconsRoot, 'normal.ts') },
-      { find: /^@ultra-ui\/icons\/colorful$/, replacement: resolve(iconsRoot, 'colorful.ts') },
-      { find: /^@ultra-ui\/icons$/, replacement: resolve(iconsRoot, 'index.ts') }
-    ]
-  },
-  server: { port: 7789, host: true }
-})
+export default defineConfig({ plugins: [vue()], server: { port: 7789, host: true } })
