396509aae0586ce98d2252d422d950a6883e2f83
whj <emailwen@qq.com>
2026-05-23 17:00:12 +0800

    style(icons): shorten form icon height and simplify inner layout

--- packages/icons/src/vue/normal/form.vue
+++ packages/icons/src/vue/normal/form.vue
@@ -1,22 +1,24 @@
-<!-- @veltra/icons generated sha256:e18d0a35a32b78b3 gen:4 source:src/svg/normal/form.svg -->
+<!-- @veltra/icons generated sha256:20012f8d7f6c23c8 gen:4 source:src/svg/normal/form.svg -->
 <script setup lang="ts">
 defineOptions({ name: 'Form' })
 </script>
 <template>
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
     <rect
-      width="12"
-      height="14"
-      x="2"
-      y="1"
+      width="14"
+      height="10"
+      x="1"
+      y="3"
       fill="none"
       stroke="currentColor"
       stroke-width="1.2"
-      rx="2"
+      rx="1.5"
     />
-    <path stroke="currentColor" stroke-linecap="round" stroke-width="1.2" d="M4 4.5h2m-2 4h2" />
-    <rect width="5" height="2" x="7" y="3.5" fill="none" stroke="currentColor" rx=".5" />
-    <rect width="5" height="2" x="7" y="7.5" fill="none" stroke="currentColor" rx=".5" />
-    <rect width="8" height="2" x="4" y="11.5" fill="currentColor" rx="1" />
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-width="1.2"
+      d="M3.5 6h2m2 0h5m-9 4h2m2 0h5"
+    />
   </svg>
 </template>
